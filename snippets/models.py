4b33a407140b22c95046fc2e833a6581c64ab30f
rahulks <rahulrahu151999@gmail.com>
2026-02-20 09:46:41 +0530

    Added psycopg installation and created models along with their migration for saving snippets and their tags

---
+++ snippets/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Tag(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Snippet(models.Model):
+    title = models.CharField(max_length=200)
+    note = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        indexes = [
+            models.Index(fields=["created_by", "created_at"]),
+        ]
+
+    def __str__(self):
+        return self.title
